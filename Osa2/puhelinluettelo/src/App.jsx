c3ca0fd2243c929189dcdf498ace658b1e177a5d
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-09-28 16:16:43 +0300

    2.11

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/filter.jsx'
 import PersonForm from './components/personForm.jsx'
 import Persons from './components/persons.jsx'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName,setFilterName] = useState('')
-  
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
+
   const addName = (event) => {
     event.preventDefault()
     const nameExists = persons.some((person) => person.name === newName)
