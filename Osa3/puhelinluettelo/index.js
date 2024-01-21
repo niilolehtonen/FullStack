7f28ad4bfb6ef79d0d6efe8232684c201196b8bf
niilolehtonen <niilo.i.lehtonen@gmail.com>
2024-01-21 22:03:48 +0200

    ex. 3.3-3.8

--- Osa3/puhelinluettelo/index.js
+++ Osa3/puhelinluettelo/index.js
@@ -1,41 +1,98 @@
-const express = require('express')
-const app = express()
+const { response } = require("express");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.json());
+morgan.token("body", function (req) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-        },
-        {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-        },
-        {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-        },
-        {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-        }
-  ]
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
-})
+const generateId = () => {
+  return Math.floor(Math.random() * 1000) + 1;
+};
 
-app.get('/info', (req,res) => {
-    const numberofPersons = persons.length
-    const infoMessage = `Phonebook has info for ${numberofPersons} people`
-    const requestTime = new Date()
-    res.writeHead(200, 'Content-Type', 'text/plain')
-    res.end(`${infoMessage}\n${requestTime}`)
-})
-  
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const numberofPersons = persons.length;
+  const infoMessage = `Phonebook has info for ${numberofPersons} people`;
+  const requestTime = new Date();
+  res.writeHead(200, "Content-Type", "text/plain");
+  res.end(`${infoMessage}\n${requestTime}`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  if (persons.find((p) => p.name === body.name)) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
